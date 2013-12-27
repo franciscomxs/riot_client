45296d881b967dffa4e72849ceac87650df292ae
Francisco Martins <franciscomxs@gmail.com>
2013-12-27 19:29:23 -0200

    Import missing files

--- lib/riot_client.rb
+++ lib/riot_client.rb
@@ -1,6 +1,12 @@
 require "riot_client/version"
 require "riot_client/configuration"
 
+require 'riot_client/services/service'
+require "riot_client/services/champion_service"
+
+require 'riot_client/models/base_model'
+require 'riot_client/models/champion'
+
 module RiotClient
   class << self
     attr_accessor :configuration
